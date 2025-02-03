0ab3ec41f7cbfa8fc2f68eff4d3adcc561f14b72
Christina Phillips <christina.phillips9838@gmail.com>
2025-02-03 17:12:13 -0500

    Changed background and experience descriptions

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { MdKeyboardArrowUp } from "react-icons/md";
 import { ExperienceItem } from "./ExperienceItem";
+import { ExperienceDescription } from "../data/ExperienceDescription";
 
 export function Experience() {
   const [collapse, setCollapsed] = useState(true);
@@ -24,9 +25,19 @@ export function Experience() {
           timePeriod={"Sept 2023 - Present"}
           jobTitle={"Software Engineer"}
           company={"The Metropolitan Transportation Authority"}
-          technologies={["C#", "React", "TypeScript", "React Native", "JavaScript", "Expo", "SQL", "PostgreSQL", "Oracle"]}
-          experienceDescription={"Build, style and maintain mobile and web applications to modernize the certification test process for train engineers, thus expiditing the certification process, reducing administrative overhead, and improving accessibility for managers in the field. Additionally, maintain and style legacy applications, adhering to the needs of application users. "}
-          className={"border-bottom"}
+          technologies={[
+            "C#",
+            "React",
+            "TypeScript",
+            "React Native",
+            "JavaScript",
+            "Expo",
+            "SQL",
+            "PostgreSQL",
+            "Oracle",
+          ]}
+          experienceDescription={ExperienceDescription.MTA}
+          className={"border-bottom border-dark"}
         />
 
         <ExperienceItem
@@ -34,9 +45,7 @@ export function Experience() {
           jobTitle={"Software Engineer Intern"}
           company={"The Trade Desk"}
           technologies={["C#", "SQL", "Oracle"]}
-          experienceDescription={
-            "Optimized the identification of misconfigured advertisement deals by expanding the qualifications for when a deal is defined as misconfigured and optimizing the SQL queries responsible for identifying deals that have become misconfigured. These enhancements allowed for advertisement deals to be identified quicker, thus allowing clients to be notified and given the chance to ammend misconfigured advertisement deals much sooner."
-          }
+          experienceDescription={ExperienceDescription.TTD}
         />
       </div>
     </div>
--- src/components/ExperienceItem.tsx
+++ src/components/ExperienceItem.tsx
@@ -13,7 +13,7 @@ export interface IExperienceItem {
 export function ExperienceItem(props: IExperienceItem) {
   return (
     <div className={`ms-5 exp ${props.className ? props.className : ""}`}>
-        <p className="fw-bold exp-title">{props.timePeriod} <GoDotFill/> {props.jobTitle} <GoDotFill/> {props.company}</p>
+        <p className="fw-bold exp-title">{props.timePeriod} <GoDotFill/> {props.jobTitle} {/*<GoDotFill/> {props.company}*/}</p>
         <TechnologyList technologies={props.technologies}/>
       <div className="ms-5 exp-desc">
         <p>{props.experienceDescription}</p>
